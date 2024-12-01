ef33370104cc79fea8e9bf519f5c3d4e1caee50b
Polina <polina.sapego21@icloud.com>
2024-12-01 18:03:30 +0300

    fix import ForYou/Carousel.tsx delete forYou/Carousel

--- src/components/ForYou/Carousel.tsx
+++ src/components/ForYou/Carousel.tsx
@@ -15,10 +15,9 @@ interface IForYou {
 }
 
 function Carousel({ posts }: IForYou) {
-  const [pages, setPages] = useState<React.ReactNode[]>([]);
   const [offset, setOffset] = useState(0);
   const [visibleIndex, setVisibleIndex] = useState(0);
-  const pageWidth = ((window.innerWidth * 52.1) / 100);
+  const pageWidth = ((window.innerWidth * 47.5) / 100);
 
   const handleLeftArrowClick = () => {
     setOffset((currentOffset) => {
@@ -30,61 +29,51 @@ function Carousel({ posts }: IForYou) {
   const handleRightArrowClick = () => {
     setOffset((currentOffset) => {
       const newOffset = currentOffset - pageWidth;
-      const maxOffset = -(pageWidth * (pages.length - 1));
+      const maxOffset = -(pageWidth * (posts.length - 1));
       return Math.max(newOffset, maxOffset);
     });
   };
 
   useEffect(() => {
-    const pageElements = posts.map((postItem, index) => (
-      <div
-        key={postItem.id}
-        className={`carousel-page ${index === visibleIndex ? 'active' : ''}`}
-        style={{
-          paddingLeft: index === 0 ? '210px' : 'none',
-        }}
-      >
-        <div
-          className="page"
-          style={{
-            width: index === visibleIndex ? '95%' : '90%',
-            marginTop: index === visibleIndex ? '2%' : '5%',
-            boxShadow: index === visibleIndex ? '-20px 20px 20px 0px rgba(255, 217, 25, 1)' : 'none',
-          }}
-        >
-          <div className="image-container">
-            <img src={PostCarousel} className="page-image" alt="carousel" />
-            <div className="page-content">
-              <h1>{postItem.title}</h1>
-              <h2>
-                <span style={{ marginRight: '10px' }}>
-                  {`${postItem.id}+ |`}
-                </span>
-                <span>{postItem.albumId}</span>
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    ));
-
-    setPages(pageElements);
-  }, [posts, visibleIndex]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const centerIndex = Math.round(Math.abs(offset) / pageWidth);
-      setVisibleIndex(centerIndex);
-    };
-
-    handleScroll();
-  }, [offset]);
+    const centerIndex = Math.round(Math.abs(offset) / pageWidth);
+    setVisibleIndex(centerIndex);
+  }, [offset, pageWidth]);
 
   return (
     <div className="main-carousel">
       <div className="window">
         <div className="all-pages-container" style={{ transform: `translateX(${offset}px)` }}>
-          {pages}
+          {posts.map((postItem, index) => (
+            <div
+              key={postItem.id}
+              className={`carousel-page ${index === visibleIndex ? 'active' : ''}`}
+              style={{
+                paddingLeft: index === 0 ? '210px' : 'none',
+              }}
+            >
+              <div
+                className="page"
+                style={{
+                  width: index === visibleIndex ? '95%' : '90%',
+                  marginTop: index === visibleIndex ? '2%' : '5%',
+                  boxShadow: index === visibleIndex ? '-20px 20px 20px 0px rgba(255, 217, 25, 1)' : 'none',
+                }}
+              >
+                <div className="image-container">
+                  <img src={PostCarousel} className="page-image" alt="carousel" />
+                  <div className="page-content">
+                    <h1>{postItem.title}</h1>
+                    <h2>
+                          <span style={{ marginRight: '10px' }}>
+                              {`${postItem.id}+ |`}
+                            </span>
+                          <span>{postItem.albumId}</span>
+                        </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <FaChevronLeft className="arrow-left" onClick={handleLeftArrowClick} />
