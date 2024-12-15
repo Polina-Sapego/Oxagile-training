f6ebce45394d9fa36d2b57233d59defafcbf165b
Polina <polina.sapego21@icloud.com>
2024-12-15 22:00:37 +0300

    change imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Settings from './components/Settings';
-import UserProfile from './components/Settings/UserProfile';
+import Settings from './components/settings';
+import UserProfile from './components/settings/UserProfile';
 import ForYou from './components/ForYou';
 import MainLayout from './layouts/MainLayout';
-import UserAgreement from './components/Settings/UserAgreement';
+import UserAgreement from './components/settings/UserAgreement';
 
 function App() {
   return (
